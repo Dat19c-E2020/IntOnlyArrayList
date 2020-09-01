69a38ca9e094baa1bf36b9eea862fc3fe5c6a4e3
JarlTuxen <jarl.tuxen@gmail.com>
2020-09-01 12:38:52 +0200

    initial commit

---
+++ src/dk/kea/dat19c/IntOnlyArrayList.java
@@ -0,0 +1,4 @@
+package dk.kea.dat19c;
+
+public class IntOnlyArrayList {
+}
---
+++ src/dk/kea/dat19c/Main.java
@@ -0,0 +1,9 @@
+package dk.kea.dat19c;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
